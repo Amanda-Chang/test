4194048ccf59a1bf6fbd525761700192d0366e73
Amanda-Chang <78400863+Amanda-Chang@users.noreply.github.com>
2021-03-28 14:18:58 +0800

    Add files via upload

---
+++ guest.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// #include<pair>
+// using namespace std;
+// int main(){
+//   int a,b;
+//   char c;
+//   cin<<a<<b;
+//   int x[a][b];
+//   pair<int,int> p;
+//   for(int k;k<a;k++){
+//     fot(int j;j<b;j++){
+//       cin>>x[k][j];
+      
+//     }
+//   }
+// }
